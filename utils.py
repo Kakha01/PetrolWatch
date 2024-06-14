ea6d7d5c6b881120f5cae8e24750eb81aff118df
Kakha <k.diasamidze2005@gmail.com>
2024-06-14 10:26:18 +0400

    fix: refactor fuel type and grade extraction and normalization in utils for readablity and modularity

--- utils.py
+++ utils.py
@@ -1,46 +1,57 @@
 import re
  
 class Utils:
+    
+    FUEL_TYPE_REGEX = r"(regular|diesel|dizel|super|premium)"
+    FUEL_GRADE_REGEX = r"(euro|evro)"
+
+    """ Mapping Georgian latinized words for fuel types and grades """
+    FUEL_TYPE_MAPPING = {
+        "dizel": "diesel",
+    }
+
+    FUEL_GRADE_MAPPING = {
+        "evro": "euro",
+    }
+
     @staticmethod
     def sluggify_fuel(fuel: str) -> str | None:
-        fuel_type_match = Utils.extract_fuel_type(fuel)
+        fuel_type = Utils.extract_fuel_type(fuel)
         
+        if not fuel_type:
+            return None
+
+        fuel_grade = Utils.extract_fuel_grade(fuel)
+
+        if not fuel_grade:
+            return fuel_type
+        
+        return f"{fuel_grade}_{fuel_type}"
+
+    @staticmethod
+    def extract_fuel_type(input: str) -> str | None:
+        fuel_type_match = re.search(Utils.FUEL_TYPE_REGEX, input, re.IGNORECASE)
+
         if not fuel_type_match:
             return None
 
-        euro, fuel_type = fuel_type_match
-
-        if euro:
-            return f"{euro}_{fuel_type}"
-        
-        return fuel_type
+        return Utils.normalize_fuel_type(fuel_type_match.group()) if fuel_type_match else None
 
     @staticmethod
-    def extract_fuel_type(fuel: str) -> tuple[str | None, str] | None:
-        euro_regex = r"(euro|evro)"
-        fuel_regex = r"(regular|diesel|dizel|super|premium)"
+    def extract_fuel_grade(input: str) -> str | None:
+        fuel_grade_match = re.search(Utils.FUEL_GRADE_REGEX, input, re.IGNORECASE)
 
-        euro_match = re.search(euro_regex, fuel, re.IGNORECASE)
-        fuel_match = re.search(fuel_regex, fuel, re.IGNORECASE)
-        
-        if not fuel_match:
+        if not fuel_grade_match:
             return None
 
-        return (
-            Utils.normalize_euro(euro_match.group()) if euro_match else None,
-            Utils.normalize_fuel(fuel_match.group())
-        )
+        return Utils.normalize_fuel_grade(fuel_grade_match.group())
 
     @staticmethod
-    def normalize_euro(euro: str) -> str:
-        """
-        replace "evro" if it exists with "euro" and convert to lowercase
-        """
-        return re.sub("evro", "euro", euro.lower())
+    def normalize_fuel_grade(fuel_grade: str) -> str:
+        fuel_grade_l = fuel_grade.lower()
+        return Utils.FUEL_GRADE_MAPPING.get(fuel_grade_l, fuel_grade_l)
     
     @staticmethod
-    def normalize_fuel(fuel: str) -> str:
-        """ 
-        replace "dizel" if it exists with "diesel" and convert to lowercase 
-        """
-        return re.sub("dizel", "diesel", fuel.lower())
+    def normalize_fuel_type(fuel_type: str) -> str:
+        fuel_type_l = fuel_type.lower()
+        return Utils.FUEL_TYPE_MAPPING.get(fuel_type_l, fuel_type_l)
