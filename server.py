5c1a91ba1b97e555953c3ebc2c5496b289d9d044
Kakha <k.diasamidze2005@gmail.com>
2024-06-20 12:39:53 +0400

    chore: refactor

--- server.py
+++ server.py
@@ -8,5 +8,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', heading_text="Hello World!")
 
+
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port="8080")
--- utils_test.py
+++ utils_test.py
@@ -1,42 +1,40 @@
 import unittest
-from utils import Utils
+import utils
+
 
 class TestUtils(unittest.TestCase):
     def test_sluggify_socar_euro_regular(self):
-        fuel = "Nano Euro Regular"
-        expected = "euro_regular"
-        result = Utils.sluggify_fuel(fuel)
-        self.assertEqual(result, expected)
+        result = utils.sluggify_fuel("Nano Euro Regular")
+        self.assertEqual(result, "euro_regular")
 
     def test_sluggify_rompetrol_euro_diesel(self):
-        fuel = "efix Euro Diesel"
-        expected = "euro_diesel"
-        result = Utils.sluggify_fuel(fuel)
-        self.assertEqual(result, expected)
+        result = utils.sluggify_fuel("efix Euro Diesel")
+        self.assertEqual(result, "euro_diesel")
 
     def test_sluggify_georgian_euro_regular(self):
-        fuel = "Evro Regulari"
-        expected = "euro_regular"
-        result = Utils.sluggify_fuel(fuel)
-        self.assertEqual(result, expected)
+        result = utils.sluggify_fuel("Evro Regulari")
+        self.assertEqual(result, "euro_regular")
 
     def test_sluggify_3(self):
-        fuel = "Hello World"
-        expected = None
-        result = Utils.sluggify_fuel(fuel)
-        self.assertEqual(result, expected)
-    
+        result = utils.sluggify_fuel("Hello World")
+        self.assertEqual(result, None)
+
     def test_sluggify_euro_premium(self):
-        fuel = "Nano Evro Premiumi"
-        expected = "euro_premium"
-        result = Utils.sluggify_fuel(fuel)
-        self.assertEqual(result, expected)
-    
+        result = utils.sluggify_fuel("Nano Evro Premiumi")
+        self.assertEqual(result, "euro_premium")
+
     def test_sluggify_gulf_super(self):
-        fuel = "G-Force Super"
-        expected = "super"
-        result = Utils.sluggify_fuel(fuel)
-        self.assertEqual(result, expected)
+        result = utils.sluggify_fuel("G-Force Super")
+        self.assertEqual(result, "super")
+
+    def test_extract_fuel_type_regular(self):
+        result = utils.extract_fuel_type("Nano Euro Regular")
+        self.assertEqual(result, "regular")
+
+    def test_extract_fuel_grade_euro(self):
+        result = utils.extract_fuel_grade("g-force evro regulari")
+        self.assertEqual(result, "euro")
+
 
 if __name__ == "__main__":
     unittest.main()
