b427cdea69597fb079e7c587efe5c7f468156221
Kakha <k.diasamidze2005@gmail.com>
2024-06-20 12:38:50 +0400

    chore: refactor fuel utils to just functions instead of static methods

--- utils.py
+++ utils.py
@@ -1,57 +1,54 @@
 import re
 
-class Utils:
+FUEL_TYPE_REGEX = r"(regular|diesel|dizel|super|premium)"
+FUEL_GRADE_REGEX = r"(euro|evro)"
 
-    FUEL_TYPE_REGEX = r"(regular|diesel|dizel|super|premium)"
-    FUEL_GRADE_REGEX = r"(euro|evro)"
+# Mapping Georgian latinized words for fuel types and grades
+FUEL_MAPPING = {
+    "dizel": "diesel",
+    "evro": "euro"
+}
 
-    """ Mapping Georgian latinized words for fuel types and grades """
-    FUEL_TYPE_MAPPING = {
-        "dizel": "diesel",
-    }
 
-    FUEL_GRADE_MAPPING = {
-        "evro": "euro",
-    }
+def sluggify_fuel(fuel: str) -> str | None:
+    fuel_type = extract_fuel_type(fuel)
 
-    @staticmethod
-    def sluggify_fuel(fuel: str) -> str | None:
-        fuel_type = Utils.extract_fuel_type(fuel)
+    if not fuel_type:
+        return None
 
-        if not fuel_type:
-            return None
+    fuel_grade = extract_fuel_grade(fuel)
 
-        fuel_grade = Utils.extract_fuel_grade(fuel)
+    if not fuel_grade:
+        return fuel_type
 
-        if not fuel_grade:
-            return fuel_type
+    return f"{fuel_grade}_{fuel_type}"
 
-        return f"{fuel_grade}_{fuel_type}"
 
-    @staticmethod
-    def extract_fuel_type(input: str) -> str | None:
-        fuel_type_match = re.search(Utils.FUEL_TYPE_REGEX, input, re.IGNORECASE)
+def extract_fuel_type(input: str) -> str | None:
+    fuel_type_match = re.search(
+        FUEL_TYPE_REGEX, input, re.IGNORECASE)
 
-        if not fuel_type_match:
-            return None
+    if not fuel_type_match:
+        return None
 
-        return Utils.normalize_fuel_type(fuel_type_match.group())
+    return normalize_fuel_type(fuel_type_match.group())
 
-    @staticmethod
-    def extract_fuel_grade(input: str) -> str | None:
-        fuel_grade_match = re.search(Utils.FUEL_GRADE_REGEX, input, re.IGNORECASE)
 
-        if not fuel_grade_match:
-            return None
+def extract_fuel_grade(input: str) -> str | None:
+    fuel_grade_match = re.search(
+        FUEL_GRADE_REGEX, input, re.IGNORECASE)
 
-        return Utils.normalize_fuel_grade(fuel_grade_match.group())
+    if not fuel_grade_match:
+        return None
 
-    @staticmethod
-    def normalize_fuel_grade(fuel_grade: str) -> str:
-        fuel_grade_l = fuel_grade.lower()
-        return Utils.FUEL_GRADE_MAPPING.get(fuel_grade_l, fuel_grade_l)
+    return normalize_fuel_grade(fuel_grade_match.group())
 
-    @staticmethod
-    def normalize_fuel_type(fuel_type: str) -> str:
-        fuel_type_l = fuel_type.lower()
-        return Utils.FUEL_TYPE_MAPPING.get(fuel_type_l, fuel_type_l)
+
+def normalize_fuel_grade(fuel_grade: str) -> str:
+    fuel_grade_l = fuel_grade.lower()
+    return FUEL_MAPPING.get(fuel_grade_l, fuel_grade_l)
+
+
+def normalize_fuel_type(fuel_type: str) -> str:
+    fuel_type_l = fuel_type.lower()
+    return FUEL_MAPPING.get(fuel_type_l, fuel_type_l)
