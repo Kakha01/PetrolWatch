443de510bd9362f1faf335318bef551e12974df6
Kakha <k.diasamidze2005@gmail.com>
2024-07-31 12:41:31 +0400

    chore: apply best practices to utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,5 @@
 import re
+from typing import Optional
 
 FUEL_TYPE_REGEX = r"(regular|diesel|dizel|super|premium)"
 FUEL_GRADE_REGEX = r"(euro|evro)"
@@ -7,47 +8,31 @@ FUEL_GRADE_REGEX = r"(euro|evro)"
 FUEL_MAPPING = {"dizel": "diesel", "evro": "euro"}
 
 
-def sluggify_fuel(fuel: str) -> str | None:
+def sluggify_fuel(fuel: str) -> Optional[str]:
     fuel_type = extract_fuel_type(fuel)
-
     if not fuel_type:
         return None
 
     fuel_grade = extract_fuel_grade(fuel)
-
-    if not fuel_grade:
-        return fuel_type
-
-    return f"{fuel_grade}_{fuel_type}"
+    return f"{fuel_grade}_{fuel_type}" if fuel_grade else fuel_type
 
 
-def extract_fuel_type(input: str) -> str | None:
-    fuel_type_match = re.search(FUEL_TYPE_REGEX, input, re.IGNORECASE)
-
-    if not fuel_type_match:
-        return None
-
-    return normalize_fuel_type(fuel_type_match.group())
+def extract_fuel_term(input: str, regex: str) -> Optional[str]:
+    match = re.search(regex, input, re.IGNORECASE)
+    return normalize_fuel_term(match.group()) if match else None
 
 
-def extract_fuel_grade(input: str) -> str | None:
-    fuel_grade_match = re.search(FUEL_GRADE_REGEX, input, re.IGNORECASE)
-
-    if not fuel_grade_match:
-        return None
-
-    return normalize_fuel_grade(fuel_grade_match.group())
+def normalize_fuel_term(term: str) -> str:
+    return FUEL_MAPPING.get(term.lower(), term.lower())
 
 
-def normalize_fuel_grade(fuel_grade: str) -> str:
-    fuel_grade_l = fuel_grade.lower()
-    return FUEL_MAPPING.get(fuel_grade_l, fuel_grade_l)
+def extract_fuel_type(input: str) -> Optional[str]:
+    return extract_fuel_term(input, FUEL_TYPE_REGEX)
 
 
-def normalize_fuel_type(fuel_type: str) -> str:
-    fuel_type_l = fuel_type.lower()
-    return FUEL_MAPPING.get(fuel_type_l, fuel_type_l)
+def extract_fuel_grade(input: str) -> Optional[str]:
+    return extract_fuel_term(input, FUEL_GRADE_REGEX)
 
 
 def capitalize_words(input: str) -> str:
-    return " ".join([word.capitalize() for word in input.split()])
+    return " ".join(word.capitalize() for word in input.split())
